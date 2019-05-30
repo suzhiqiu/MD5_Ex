371554e72bd7636e9f42c8c7bb4247c3ce25f973
suzhiqiu <364121248@qq.com.com>
2019-05-31 07:57:24 +0800

    提交子目录

---
+++ MD5_Ex/subModel/NSString+MD5.h
@@ -0,0 +1,22 @@
+//
+//  NSString+MD5.h
+//  MD5Demo_Example
+//
+//  Created by suzhiqiu on 2019/5/30.
+//  Copyright © 2019 364121248. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CommonCrypto/CommonCryptor.h>
+#import <CommonCrypto/CommonDigest.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (MD5)
+
+//生成md5
+- (NSString *)sd_md5;
+
+@end
+
+NS_ASSUME_NONNULL_END
